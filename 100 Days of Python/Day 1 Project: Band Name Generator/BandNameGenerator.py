2e6322925b6b220a4ef71c70f3e60d60d43bb85d
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-08-11 21:30:11 +0300

    Update BandNameGenerator.py

--- 100 Days of Python/Day 1 Project: Band Name Generator/BandNameGenerator.py
+++ 100 Days of Python/Day 1 Project: Band Name Generator/BandNameGenerator.py
@@ -1,1 +1,4 @@
-
+print("Welcome to the Band Name Generator.")
+x = input("What's the name of the city you grew up in? \n")
+y = input("What's your pet's name? \n")
+print("Your Band Name could be " + x + ' ' + y)
