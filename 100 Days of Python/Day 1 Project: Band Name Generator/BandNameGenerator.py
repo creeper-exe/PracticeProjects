d2ce7b7a173c72ba0823b9153286c9b211a55724
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-08-11 21:33:06 +0300

    Update BandNameGenerator.py

--- 100 Days of Python/Day 1 Project: Band Name Generator/BandNameGenerator.py
+++ 100 Days of Python/Day 1 Project: Band Name Generator/BandNameGenerator.py
@@ -1,4 +1,11 @@
+#A simple app that generates band name that consist of two inputs (When you were born and your first pet name)
+
+#It display the question
 print("Welcome to the Band Name Generator.")
+
+#Two variables that store the user input
 x = input("What's the name of the city you grew up in? \n")
 y = input("What's your pet's name? \n")
+
+#Prints the output
 print("Your Band Name could be " + x + ' ' + y)
