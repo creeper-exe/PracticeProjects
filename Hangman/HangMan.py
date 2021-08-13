3fef9d7ea64a237b68daa2761e8dd9b111bc0c00
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-08-13 19:35:41 +0300

    Update HangMan.py

--- Hangman/HangMan.py
+++ Hangman/HangMan.py
@@ -1,14 +1,16 @@
-import random
+import random #We will need this library to randomize chooising the word from the word list
 
-with open("wordlist.txt", 'r') as f:
-    words = f.readlines()
+#choosing the word from the wordlist
+with open("wordlist.txt", 'r') as f: #change the wordlist file name to the wordlist file name you wanna add
+    words = f.readlines() 
 
-word = random.choice(words)[:-1]
+word = random.choice(words)[:-1] #we use this so it doesnt count the back slash at the end of each line
 
-allowed_mistakes = 6
+allowed_mistakes = int(input("what is the number of mistakes allowed (low number means high diffculty): ")) #number of mistakes allowed
 guesses = []
 done = False
 
+#it print _ depending on the numbeer of letters in the word
 while not done:
     for letter in word:
         if letter.lower() in guesses:
@@ -17,7 +19,8 @@ while not done:
             print("_", end=" ")    
     print("")
     
-    
+
+    #decreases the allowed mistakes by one if the answer is wrong and when it reaches the zero it print game over, and if the answer was right it saves it until the user types all the right letters then it displays that he won
     guess = input(f"Allowed Mistakes Left {allowed_mistakes}, Next Guess:")
     guesses.append(guess.lower())
     if guess.lower() not in word.lower():
