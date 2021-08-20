8b574fd3ad6ab6cb71904745e13845a39f516d78
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-08-20 21:21:16 +0300

    Create StarTriangle.cpp

---
+++ C++ Learning Journey/StarTriangle.cpp
@@ -0,0 +1,20 @@
+//Star Triangle
+#include <iostream>
+
+int main()
+{
+    int rows;
+
+    std::cout << "Enter number of rows: ";
+    std::cin >> rows;
+
+    for(int i=1; i<=rows; i++)
+    {
+        for(int b=rows-i; b>0; b--)
+            std::cout << " ";
+        for(int s=1; s<=i; s++)
+            std::cout << "* ";
+        std::cout << std::endl;
+        
+    }
+}
