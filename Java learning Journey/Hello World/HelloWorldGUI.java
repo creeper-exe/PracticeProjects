86bfede1b164fcaba80e97b3def0d75c6b9f78e9
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-10-11 13:37:44 +0300

    Update HelloWorldGUI.java

--- Java learning Journey/Hello World/HelloWorldGUI.java
+++ Java learning Journey/Hello World/HelloWorldGUI.java
@@ -1,1 +1,19 @@
+import javax.swing.JOptionPane;
 
+public class Main {
+    
+    public static void main(String[] args) {
+        //Input for name
+        String name = JOptionPane.showInputDialog("Enter your Name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+    
+        //Input for age
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your Age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+
+        //Input for Height
+        Double Height = Double.parseDouble(JOptionPane.showInputDialog("Enter your Height"));
+        JOptionPane.showMessageDialog(null, "Your Height is "+Height );
+
+    }
+}
